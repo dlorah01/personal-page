2a4842701b6069b5feb96938a55bdcc14b7d5193
Harold Garcia <hj.garcia652@gmail.com>
2023-05-14 22:26:05 -0500

    preview details done

--- src/app/common/window-types/preview/preview.component.ts
+++ src/app/common/window-types/preview/preview.component.ts
@@ -1,14 +1,50 @@
-import { AfterViewInit, Component, Input } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
+import { AfterViewInit, Component, Input, SimpleChanges } from '@angular/core';
 @Component({
   selector: 'app-preview',
   templateUrl: './preview.component.html',
-  styleUrls: ['./preview.component.sass']
+  styleUrls: ['./preview.component.sass'],
+  animations: [
+    trigger(
+      'inOutAnimation',
+      [
+        transition(
+          ':enter',
+          [
+            style({ opacity: 0 }),
+            animate('0.2s {{delay}}s ease-out',
+                    style({ opacity: 1}))
+          ], {params: {delay: '0'}}
+        ),
+        transition(
+          ':leave',
+          [
+            style({ opacity: 1 }),
+            animate('0.2s ease-in',
+                    style({ opacity: 0 }))
+          ]
+        )
+      ]
+    )
+  ]
 })
 export class PreviewComponent implements AfterViewInit{
   @Input() project!: any
   imgSource!: string
+  currentProportion = 'center'
+  currentFramework!: string
 
   constructor() {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("hi", changes['project'])
+    if (changes['project']) {
+     setTimeout(() => {
+      this.currentFramework = changes['project'].currentValue.framework
+
+     });
+    }
+  }
 
   getImageSrc(): string {
     return `../../../../assets/images/projects/${this.project.id}.png`;
@@ -26,5 +62,9 @@ export class PreviewComponent implements AfterViewInit{
 
   isADetail(text: string): boolean {
     return text.startsWith('- ')
+  }
+
+  changeView(proportion: string): void {
+    this.currentProportion = proportion
   }
 }
